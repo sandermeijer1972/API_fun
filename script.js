f08c261b63703b429e7526ff769be2b07f95036a
Sander Meijer <sandertje1972@hotmail.com>
2021-05-07 13:19:22 +0200

    added files

---
+++ script.js
@@ -0,0 +1,24 @@
+const newJoke = document.getElementById('grap');
+const clickButton = document.getElementById('nieuwegrap');
+
+const getJoke = async function() {
+    const apiURL = `https://icanhazdadjoke.com/`;
+    try {
+        const res = await fetch(apiURL, {headers: {Accept: "application/json"}});
+        const data = await res.json();
+        console.log("willekeurige grap: ", data);
+        return data.joke;
+    } catch(error) {
+        console.log(error);
+    }
+};
+
+const addJokeToDOM = async function() {
+    const joke = await getJoke();
+    newJoke.innerHTML=(joke);
+};
+
+clickButton.addEventListener("click", addJokeToDOM);
+
+
+
