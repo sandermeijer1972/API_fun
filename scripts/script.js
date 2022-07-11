00e76e2f90f20afb7d5d5bc4778ee6cfa257fcd0
Sander Meijer <sandertje1972@hotmail.com>
2022-07-11 11:36:38 +0200

    added better transitions to data-link

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,7 @@ const laatKnoppenZien = function() {
     setTimeout(() => {
         uitlegText.innerHTML = '';
         body.classList.add("dark");
+        body.classList.add("startscherm");
     }, 4000);
     setTimeout(() => {
         knoppen.classList.add("knopaan");
@@ -18,7 +19,7 @@ const laatKnoppenZien = function() {
         newP.classList.add("uitleg");
         newP.innerText = "Helaas werken niet alle knoppen even goed. Om de grootste fouten op te vangen, klik dan eerst even op onderstaande link. Je komt dan op een andere pagina terecht, waar je even 'request temporary access to the demo server' moet aanvragen. Daarna kan je hierboven in principe elke knop zo vaak aanklikken als je zelf wil.";
         result.appendChild(newP);        
-    }, 9000);
+    }, 8000);
     setTimeout(() => {
         const newPLink = document.createElement('p');
         newPLink.classList.add("link");
@@ -27,13 +28,18 @@ const laatKnoppenZien = function() {
         newA.href = "https://cors-anywhere.herokuapp.com/corsdemo";
         newA.innerText = "===> klik voor toegang tot de data <===";
         newPLink.appendChild(newA);
-        result.appendChild(newPLink); 
-    }, 10000);
+        result.appendChild(newPLink);         
+    }, 8500);
+    setTimeout(() => {
+        result.classList.add("start");
+    }, 9000);
 };
 const button = document.getElementById("uitleg3");
 button.addEventListener("click", laatKnoppenZien);
 
 const emptyResults = () => {
+    body.classList.remove("startscherm");
+    result.classList.remove("start");
     result.classList.remove("hondenfoto");
     result.classList.remove("grappenmaker");
     result.classList.remove("gerecht");
